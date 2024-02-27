4aa23239025ec55fe49286dc1b694de9c64021f3
abdurrahman4127 <whoamiiammr.nobody@gmail.com>
2024-02-27 10:12:28 +0600

    code added

---
+++ src/components/test-component/test-component.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+
+const TestComponent = () => {
+  const products = [
+    {
+      name: "Tide Powder",
+      image: "frontend/src/components/img/Tide_Logo_RGB_2014.png", 
+      description:
+        "Washing powder. Tide is an American brand of laundry. Washing powder. Tide is an American brand of laundry",
+    }
+  ];
+
+  return (
+    <div className="product-container">
+      <h1>E Commerce</h1>
+      <div className="product-list">
+        {products.map((product) => (
+          <div key={product.name} className="product">
+            <img src={product.image} alt={product.name} className="product-image" />
+            <div className="product-info">
+              <h3>{product.name}</h3>
+              <p className="product-description">{product.description}</p>
+            </div>
+          </div>
+        ))}
+      </div>
+
+      <div className="product-list">
+        {products.map((product) => (
+          <div key={product.name} className="product">
+            <img src={product.image} alt={product.name} className="product-image" />
+            <div className="product-info">
+              <h3>{product.name}</h3>
+              <p className="product-description">{product.description}</p>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default TestComponent;
